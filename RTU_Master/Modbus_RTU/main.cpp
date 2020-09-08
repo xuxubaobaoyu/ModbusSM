f4b82c50cbfcbdddf55775533e73cdcf82ea356a
xuxubaobaoyu <1656567238@qq.com>
2020-09-08 08:46:39 +0800

    no message

--- RTU_Master/Modbus_RTU/main.cpp
+++ RTU_Master/Modbus_RTU/main.cpp
@@ -15,27 +15,23 @@ int main()
 	ModbusRTUDataInit(&ModbusRTUWData);//进行初始化例如ID、Function等
 	//初始化串口
 	hCom = InitCOM("COM1", ModbusRTUWData.TimeOuts);//端口号、通信超时时间
-	while (1)
-	{
+	while (1){
 		SpaceIsTrue();
 		//发送查询报文
 		int length = ModbusRTUQueryMessage(WriteBUF, &ModbusRTUWData);
 		ComWrite(hCom, WriteBUF, length);//发送数据
-		if (ModbusRTUWData.ID != 0)//判断是否是广播，以此判断需不需要读取返回值
-		{
+		if (ModbusRTUWData.ID != 0){//判断是否是广播，以此判断需不需要读取返回值
 			BYTE ReadBuf[300];//用于读取缓冲
 			int len = ReadBufLength(&ModbusRTUWData);//先计算响应报文的长度,设备ID为0,即广播就不用读取
 			bool b = false;
 			b = ComRead(hCom, ReadBuf, len);//端口号、存储数组、访问字节数//用于判断
-			if (b)
-			{
+			if (b){
 				DecomposeMessage(WriteBUF, ReadBuf, &ModbusRTUWData);
 				printf("响应报文如下：\n");
 				for (int i = 0; i < len; i++)
 					printf("%02X ", ReadBuf[i]);
 			}
-			else
-			{
+			else{
 				printf("等待接收响应报文超时\n");
 			}
 			printf("\n");//换行
